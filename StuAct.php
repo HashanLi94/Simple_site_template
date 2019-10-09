cae1ec5b15330725a117862602391bbb2df833e2
Hashan Liyanage <55444962+HashanLi94@users.noreply.github.com>
2019-10-10 01:39:19 +0530

    Student activities page

---
+++ StuAct.php
@@ -0,0 +1,42 @@
+<?php
+    include("includes/header.php");
+?>
+        <main>
+            <article>
+                <h1>Students Activities</h1> <hr>
+                <li>
+                    <ul>
+                        <h1>On going researches</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatibus distinctio recusandae aliquam? At saepe ad necessitatibus nesciunt incidunt pariatur, totam dolor eaque obcaecati repudiandae, molestias omnis quis ipsum minus?</p>
+                    </ul>
+                    <ul>
+                        <h1>On going projects</h1><hr>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam commodi iure quisquam quos non! Labore amet ad animi aperiam, exercitationem doloremque! A non asperiores recusandae sit porro? Enim, sequi necessitatibus!</p>
+                    </ul>
+                    <ul>
+                        <h1>Inter-Universiy Games</h1><hr>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas impedit debitis cumque corporis quos deleniti ducimus, optio neque perferendis vero ad? Distinctio perspiciatis fuga ipsa minus quaerat quas cumque? Accusantium?</p>
+                    </ul>
+                    <ul>
+                        <h1>Events by the Students</h1><hr>
+                        <li>
+                            <ul>
+                                <h1>Raathriya-Wee Musical Consert</h1><hr>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, perferendis? Inventore atque iusto voluptatum veniam. Ut deleniti saepe, dolorum quos earum inventore dignissimos debitis impedit illo quis et expedita quidem.</p>
+                            </ul>
+                            <ul>
+                                <h1>Aashwaasa Consert</h1><hr>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere veritatis voluptatibus similique error amet facilis quae illo, quidem nesciunt vel sunt natus assumenda ullam non ut nostrum rem qui molestias.</p>
+                            </ul>
+                            <ul>
+                                <h1>Realey Carnival</h1><hr>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum a magni, dolorum voluptas est non eius eum quibusdam et atque doloribus, ducimus inventore corporis. Culpa magni expedita mollitia voluptates dolor.</p>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </article>
+        </main>
+<?php
+    include("includes/footer.php");
+?>        
