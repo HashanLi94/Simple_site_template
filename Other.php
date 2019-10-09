ee4dc3b59f90d4bbdf7480cf593b7474e49f4216
Hashan Liyanage <55444962+HashanLi94@users.noreply.github.com>
2019-10-10 01:38:37 +0530

    Add files via upload

---
+++ Other.php
@@ -0,0 +1,18 @@
+<?php
+    include("includes/header.php");
+?>
+
+        <main>
+            <article>
+                <h1>Other Activities-FAS-SUSL</h1> <hr>
+                <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, deleniti. Veniam adipisci consequuntur sequi nam saepe minus id, quis dignissimos, ipsum possimus eius, eveniet velit! Culpa numquam facilis optio ea!</P>
+                <P>Nesciunt beatae est soluta culpa saepe dicta quam nemo laborum eius perspiciatis repudiandae impedit placeat eligendi distinctio hic delectus commodi dolorem dolorum officia praesentium, ullam nam. Sint sapiente quaerat nisi!</P>
+                <P>Distinctio, sit consequatur fugit velit ipsa assumenda, illo iusto molestias facere atque perspiciatis ad ipsam veniam labore eligendi dignissimos, obcaecati mollitia nostrum aliquam nihil animi maxime? Repellendus iste magnam sunt!</P>
+            </article>
+        </main>
+ <?php
+    include("includes/footer.php");
+?>        
+
+
+       
